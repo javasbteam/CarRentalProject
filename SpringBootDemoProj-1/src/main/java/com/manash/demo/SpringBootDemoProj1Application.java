233f2f93251c9cef5d98e029266183f273e0c1bd
Manash.Swain <Manash.Swain@IVL-WS134.INFOVISIONLABS.COM>
2019-11-18 12:53:01 +0530

    Test commit

---
+++ SpringBootDemoProj-1/src/main/java/com/manash/demo/SpringBootDemoProj1Application.java
@@ -0,0 +1,13 @@
+package com.manash.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoProj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoProj1Application.class, args);
+	}
+
+}
