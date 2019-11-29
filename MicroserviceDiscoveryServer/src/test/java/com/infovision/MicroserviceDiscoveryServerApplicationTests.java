a1d06f5ab5d49de2866fe644de23ad229415977e
Priyanka.Ronge <Priyanka.Ronge@IVL-WS33.INFOVISIONLABS.COM>
2019-11-29 10:36:04 +0530

    Added Microservice Eureka Server Project

---
+++ MicroserviceDiscoveryServer/src/test/java/com/infovision/MicroserviceDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infovision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
