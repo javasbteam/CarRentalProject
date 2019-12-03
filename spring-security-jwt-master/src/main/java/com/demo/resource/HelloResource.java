1dc97ea681ce1e308e1112b86625b0a0bac1a5c9
Manash.Swain <Manash.Swain@IVL-WS134.INFOVISIONLABS.COM>
2019-12-03 11:07:31 +0530

    Spring Security

---
+++ spring-security-jwt-master/src/main/java/com/demo/resource/HelloResource.java
@@ -0,0 +1,60 @@
+package com.demo.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.models.AuthenticationRequest;
+import com.demo.models.AuthenticationResponse;
+import com.demo.service.MyUserDetailsService;
+import com.demo.util.JwtUtil;
+
+@RestController
+public class HelloResource {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private JwtUtil jwtTokenUtil;
+
+	@Autowired
+	private MyUserDetailsService userDetailsService;
+
+	@RequestMapping({ "/hello" })
+	public String firstPage() {
+		return "Hello World";
+
+	}
+	
+	@RequestMapping("/{name}")
+	public String getWish(@PathVariable("name") String name) {
+		return name+" Good Morning";
+	}
+
+	@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest)
+			throws Exception {
+
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+					authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+		} catch (BadCredentialsException e) {
+			throw new Exception("Incorrect username or password", e);
+		}
+
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+		final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+		return ResponseEntity.ok(new AuthenticationResponse(jwt));
+	}
+}
